b5eac6fa4bcb94a92defe936fbb935727a41cd24
MFirdausA <mfirdausakbar88@gmail.com>
2025-07-10 18:01:30 +0700

    update: delete useless code adminLayout, new font, new styling for customer pages

--- frontend/src/components/adminLayout.tsx
+++ frontend/src/components/adminLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./header";
 import Sidebar from "./sidebar";
 import { Toaster } from "sonner";
+import "../shadcn.css";
 
 
 export default function AdminLayout() {
@@ -11,9 +12,6 @@ export default function AdminLayout() {
             <div className="flex flex-col">
                 <Header />
                 <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-                    {/* <div className="flex items-center">
-                        <h1 className="text-lg font-semibold md:text-2xl"><Outlet /></h1>
-                    </div> */}
                         <h1 className="text-lg font-semibold md:text-2xl"><Outlet /></h1>
                 </main>
             </div>
